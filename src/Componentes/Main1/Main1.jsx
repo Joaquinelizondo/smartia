ebba81149c2730040a376d1c54885c684f87d819
Joaquin Elizondo <joaquin.elizondo@cencosud.com.uy>
2025-03-11 00:55:32 -0300

    correccion Main1.jsx

--- src/Componentes/Main1/Main1.jsx
+++ src/Componentes/Main1/Main1.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import './index.css'
-import fotoMain from "../../assets/foto_main1.jpg"
-
-
+import React from "react";
+import "./index.css"; 
+import fotoMain from "../../assets/foto_main1.jpg";
 
 const Main1 = () => {
   return (
     <div className="container">
       {/* 📌 Imagen en el lado izquierdo */}
       <div className="image-container">
-      <img src={fotoMain} alt="Smartialab AI Solutions" /> 
-
+        <img src={fotoMain} alt="Smartialab AI Solutions" /> 
       </div>
 
       {/* 📌 Texto en el lado derecho */}
       <div className="text-container">
-        <h2 className='title_main1'>Smartialab: AI Solutions for Business Growth</h2>
-        <p className='text_main1'>
-        At Smartialab, we harness the power of Artificial Intelligence to transform the way businesses interact with their customers. Our intelligent agents provide personalized recommendations, seamless interactions, and valuable business insights, optimizing marketing strategies and improving customer experiences.
-
-By leveraging Natural Language Processing (NLP), Data Analytics, and AI-driven automation, we help businesses enhance efficiency, drive engagement, and scale globally. Whether you're a small business or a large enterprise, Smartialab empowers you with the technology needed to stay ahead in the digital market.
+        <h2 className="title_main1">Smartialab: AI Solutions for Business Growth</h2>
+        <p className="text_main1">
+          At Smartialab, we harness the power of Artificial Intelligence to transform the way businesses interact with their customers. Our intelligent agents provide personalized recommendations, seamless interactions, and valuable business insights, optimizing marketing strategies and improving customer experiences.
+        </p>
+        <p className="text_main1">
+          By leveraging Natural Language Processing (NLP), Data Analytics, and AI-driven automation, we help businesses enhance efficiency, drive engagement, and scale globally. Whether you're a small business or a large enterprise, Smartialab empowers you with the technology needed to stay ahead in the digital market.
         </p>
       </div>
     </div>
