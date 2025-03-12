9fe1fd8679e200205d190abd0a6d5979c22883b7
Joaquin Elizondo <joaquin.elizondo@cencosud.com.uy>
2025-03-12 01:28:08 -0300

    actualizacion vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ⚠️ IMPORTANTE para que funcione en GitHub Pages
+  base: './', // ⚠️ IMPORTANTE para que funcione en GitHub Pages
 });
